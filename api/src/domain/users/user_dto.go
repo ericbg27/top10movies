437cece6330791ae290455be7083956ffa049379
ericbg27 <ericbgil@gmail.com>
2021-05-15 15:05:09 -0300

    [Modification] Changing rest error struct. Now correctly sending errors in HTTP response

--- api/src/domain/users/user_dto.go
+++ api/src/domain/users/user_dto.go
@@ -21,7 +21,7 @@ type User struct {
 	Password    string `json:"password"`
 }
 
-func (user *User) Validate() rest_errors.RestErr {
+func (user *User) Validate() *rest_errors.RestErr {
 	user.FirstName = strings.TrimSpace(user.FirstName)
 	user.LastName = strings.TrimSpace(user.LastName)
 	if user.FirstName == "" || user.LastName == "" {
--- api/src/utils/rest_errors/rest_error.go
+++ api/src/utils/rest_errors/rest_error.go
@@ -5,58 +5,44 @@ import (
 	"net/http"
 )
 
-type restErr struct {
-	message string `json:"message"`
-	status  int    `json:"status"`
-	err     string `json:"error"`
+type RestErr struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Err     string `json:"error"`
 }
 
-type RestErr interface {
-	Message() string
-	Status() int
-	Error() string
+func (r RestErr) Error() string {
+	return fmt.Sprintf("message: %s - status: %d - error: %s", r.Message, r.Status, r.Err)
 }
 
-func (r restErr) Error() string {
-	return fmt.Sprintf("message: %s - status: %d - error: %s", r.message, r.status, r.err)
-}
-
-func (r restErr) Message() string {
-	return r.message
-}
-
-func (r restErr) Status() int {
-	return r.status
-}
-
-func NewRestError(message string, status int, err string) RestErr {
-	return restErr{
-		message: message,
-		status:  status,
-		err:     err,
+func NewRestError(message string, status int, err string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  status,
+		Err:     err,
 	}
 }
 
-func NewBadRequestError(message string) RestErr {
-	return restErr{
-		message: message,
-		status:  http.StatusBadRequest,
-		err:     "bad_request",
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Err:     "bad_request",
 	}
 }
 
-func NewNotFoundError(message string) RestErr {
-	return restErr{
-		message: message,
-		status:  http.StatusNotFound,
-		err:     "not_found",
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Err:     "not_found",
 	}
 }
 
-func NewInternalServerError(message string) RestErr {
-	return restErr{
-		message: message,
-		status:  http.StatusInternalServerError,
-		err:     "internal_server_error",
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Err:     "internal_server_error",
 	}
 }
