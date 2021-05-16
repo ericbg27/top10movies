6ebac11649d8347e5868ff14ed6a48bbc67c8496
ericbg27 <ericbgil@gmail.com>
2021-05-16 11:56:10 -0300

    [Feature addition] Password now being hashed. When /login is hit, the password received is compared to the saved one

--- api/go.mod
+++ api/go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/jackc/pgx/v4 v4.11.0
 	github.com/spf13/viper v1.7.1
 	go.uber.org/zap v1.16.0
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
 )
--- api/src/controllers/users/users_controller.go
+++ api/src/controllers/users/users_controller.go
@@ -6,8 +6,10 @@ import (
 
 	"github.com/ericbg27/top10movies-api/src/domain/users"
 	users_service "github.com/ericbg27/top10movies-api/src/services/users"
+	"github.com/ericbg27/top10movies-api/src/utils/logger"
 	"github.com/ericbg27/top10movies-api/src/utils/rest_errors"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 const (
@@ -15,7 +17,31 @@ const (
 )
 
 func Login(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := rest_errors.NewBadRequestError("Invalid JSON body")
+		c.JSON(restErr.Status, restErr)
 
+		return
+	}
+
+	savedUser, getErr := users_service.UsersService.GetUser(user)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(savedUser.Password), []byte(user.Password))
+	if err != nil {
+		passwordErr := rest_errors.NewBadRequestError("Wrong password")
+		c.JSON(passwordErr.Status, passwordErr)
+
+		return
+	}
+
+	// Here we create a token and send to the user
+	c.JSON(http.StatusOK, nil)
 }
 
 func Create(c *gin.Context) {
@@ -30,12 +56,25 @@ func Create(c *gin.Context) {
 	user.Status = users.StatusActive
 	user.DateCreated = time.Now().Format(layoutISO)
 
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		logger.Error("Unable to hash password", err)
+		hashErr := rest_errors.NewBadRequestError("Unable to hash password")
+		c.JSON(hashErr.Status, hashErr)
+
+		return
+	}
+
+	user.Password = string(hashedPass)
+
 	result, saveErr := users_service.UsersService.CreateUser(user)
 	if saveErr != nil {
 		c.JSON(saveErr.Status, saveErr)
 
 		return
 	}
+
+	result.Password = ""
 
 	c.JSON(http.StatusCreated, &result)
 }
