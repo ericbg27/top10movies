02bca8b53ca80744b1c6d06cb710febeaa021013
ericbg27 <ericbgil@gmail.com>
2021-11-17 17:52:20 -0300

    [Modification] Improving search user query

--- api/src/queries/users/users_queries.go
+++ api/src/queries/users/users_queries.go
@@ -16,6 +16,6 @@ const (
 	QueryDeleteUser     = "DELETE FROM users WHERE id=$1;"
 	QueryDeleteUserName = "delete-user-query"
 
-	QuerySearchUser     = "SELECT id, first_name, last_name, email FROM users WHERE first_name ILIKE $1 OR last_name ILIKE $2;"
+	QuerySearchUser     = "SELECT id, first_name, last_name, email FROM users WHERE first_name ILIKE '' || $1 || '%' AND last_name ILIKE '%' || $2 || '%';"
 	QuerySearchUserName = "search-user-query"
 )
